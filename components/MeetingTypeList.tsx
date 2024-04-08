7ade13eac652058621a532d9e77103b5c2148df9
allenlin <ht016746@gmail.com>
2024-04-08 14:42:45 +0700

    feat: dialog for instant meeting

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -4,12 +4,16 @@ import { useState } from 'react';
 
 import HomeCard from './HomeCard';
 import { useRouter } from 'next/navigation';
+import MeetingModal from './MeetingModal';
 
 const MeetingTypeList = () => {
   const router = useRouter();
+
   const [meetingState, setMeetingState] = useState<
     'isScheduleMeeting' | 'isJoiningMeeting' | 'isInstantMeeting' | undefined
   >();
+
+  const createMeeting = () => {};
 
   return (
     <section className='grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4'>
@@ -40,6 +44,14 @@ const MeetingTypeList = () => {
         description='via invitation link'
         handleClick={() => setMeetingState('isJoiningMeeting')}
         className='bg-yellow-1'
+      />
+      <MeetingModal
+        isOpen={meetingState === 'isInstantMeeting'}
+        onClose={() => setMeetingState(undefined)}
+        title='Start an Instant Meeting'
+        className='text-center'
+        buttonText='Start Meeting'
+        handleClick={createMeeting}
       />
     </section>
   );
