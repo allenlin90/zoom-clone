9fa4dedd8f94f4945273b19d009a680c6f59997c
allenlin <ht016746@gmail.com>
2024-04-08 12:47:22 +0700

    chore: optimize syntax and identation

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -21,9 +21,7 @@ const Sidebar = () => {
               key={link.label}
               className={cn(
                 'flex gap-4 items-center p-4 rounded-lg justify-start',
-                {
-                  'bg-blue-1': isActive,
-                }
+                { 'bg-blue-1': isActive }
               )}
             >
               <Image
