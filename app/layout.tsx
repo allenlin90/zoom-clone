160238062342558e2f9dffba5266a55fbfd96caf
allenlin <ht016746@gmail.com>
2024-04-08 16:19:17 +0700

    chore: add toaster for interaction result

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import { ClerkProvider } from '@clerk/nextjs';
 
+import { Toaster } from '@/components/ui/toaster';
+
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
@@ -33,6 +35,7 @@ export default function RootLayout({
         }}
       >
         <body className={`${inter.className} bg-dark-2`}>{children}</body>
+        <Toaster />
       </ClerkProvider>
     </html>
   );
--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -5,6 +5,7 @@ import { useRouter } from 'next/navigation';
 import { useUser } from '@clerk/nextjs';
 import { Call, useStreamVideoClient } from '@stream-io/video-react-sdk';
 
+import { useToast } from '@/components/ui/use-toast';
 
 import HomeCard from './HomeCard';
 import MeetingModal from './MeetingModal';
@@ -24,11 +25,17 @@ const MeetingTypeList = () => {
     link: '',
   });
   const [callDetails, setCallDetails] = useState<Call>();
+  const { toast } = useToast();
 
   const createMeeting = async () => {
     if (!client || !user) return;
 
     try {
+      if (!values.dateTime) {
+        toast({ title: 'Please select a date and time' });
+        return;
+      }
+
       const id = crypto.randomUUID();
       const call = client.call('default', id);
 
@@ -49,8 +56,11 @@ const MeetingTypeList = () => {
       if (!values.description) {
         router.push(`/meeting/${call.id}`);
       }
+
+      toast({ title: 'Meeting created' });
     } catch (error: any) {
       console.log(error);
+      toast({ title: 'Failed to create a meeting' });
     }
   };
 
