9998151f7392045935d79198c2cf0616227d1d0e
allenlin <ht016746@gmail.com>
2024-04-09 21:34:31 +0700

    fix: handle undefined

--- components/CallList.tsx
+++ components/CallList.tsx
@@ -88,13 +88,13 @@ const CallList = ({ type }: CallList) => {
                 : '/icons/recordings.svg'
             }
             title={
-              (meeting as Call).state?.custom.description.substring(0, 26) ||
-              (meeting as CallRecording)?.filename.substring(0, 20) ||
+              (meeting as Call).state?.custom?.description?.substring(0, 26) ||
+              (meeting as CallRecording)?.filename?.substring(0, 20) ||
               'No description'
             }
             date={
               (meeting as Call).state?.startsAt?.toLocaleString() ||
-              (meeting as CallRecording)?.start_time.toLocaleString()
+              (meeting as CallRecording)?.start_time?.toLocaleString()
             }
             isPreviousMeeting={type === 'ended'}
             buttonIcon1={type === 'recordings' ? '/icons/play.svg' : undefined}
